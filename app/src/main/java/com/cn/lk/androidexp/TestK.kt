bf34d0f6e1979ecb19438431c159870523b78fa9
luokai <luokai@bluemoon.com.cn>
2018-10-12 15:11:09 +0800

    暂存

--- app/src/main/java/com/cn/lk/androidexp/TestK.kt
+++ app/src/main/java/com/cn/lk/androidexp/TestK.kt
@@ -1,5 +1,64 @@
 package com.cn.lk.androidexp
 
-class TestK {
+object TestK {
+    fun fourSum(nums: IntArray, target: Int): List<List<Int>> {
+        nums.sort()
+        val res = mutableListOf<List<Int>>()
+        for ((index, value) in nums.withIndex()) {
+            if (index == 0 || value != nums[index - 1]) {
+                val subRes = threeSum(nums, index + 1, target - value, value)
+                if (!subRes.isEmpty()) {
+                    res.addAll(subRes)
+                }
+            }
+        }
+        return res
+    }
 
+    fun threeSum(nums: IntArray, start: Int, target: Int, ext: Int): List<List<Int>> {
+        val res = mutableListOf<List<Int>>()
+        var left = start
+        var right = nums.size - 2
+        while (left < right) {
+            val subRes = twoSum(nums, left + 1, target - nums[left], nums[left], ext)
+            if (!subRes.isEmpty()) {
+                res.addAll(subRes)
+            }
+            do {
+                left++
+            } while (left < right && nums[left] == nums[left - 1])
+        }
+        return res
+    }
+
+    /**
+     * 所有的两数和集合（去重）变种
+     */
+    fun twoSum(nums: IntArray, start: Int, target: Int, vararg ext: Int): List<List<Int>> {
+        val res = mutableListOf<List<Int>>()
+        var left = start
+        var right = nums.size - 1
+        while (left < right) {
+            val sum = nums[left] + nums[right]
+            when {
+                sum < target -> do {
+                    left++
+                } while (left < right && nums[left] == nums[left - 1])
+                sum > target -> do {
+                    right--
+                } while (left < right && nums[right] == nums[right + 1])
+                else -> {
+                    var l = mutableListOf<Int>(nums[left], nums[right])
+                    for (prex in ext) {
+                        l.add(0, prex)
+                    }
+                    res.add(l)
+                    do {
+                        left++
+                    } while (left < right && nums[left] == nums[left - 1])
+                }
+            }
+        }
+        return res
+    }
 }
---
+++ app/src/test/java/com/cn/lk/androidexp/TestKTest.kt
@@ -0,0 +1,16 @@
+package com.cn.lk.androidexp
+
+import org.junit.Assert
+import org.junit.Test
+
+/**
+ * Created by lk on 2018/10/9.
+ */
+class TestKTest {
+    @Test
+    fun testTwoSum() {
+        Assert.assertEquals(listOf(listOf(-1,  0, 0, 1),listOf(-2, -1, 1, 2),listOf(-2,  0, 0, 2)),
+                TestK.fourSum(intArrayOf(1, 0, -1, 0, -2, 2), 0))
+    }
+
+}
